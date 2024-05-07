9b073f0c7b7c227283c5da465c7ad6c8f3597908
Marcus Reis <mvsantosreis@gmail.com>
2024-05-07 19:30:45 +0100

    Register and Login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,1 +1,22 @@
-<template></template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const user = ref()
+onMounted(async () => {
+  const data = await axios.get('/api/user')
+  user.value = data.data
+})
+</script>
+<template>
+  <div class="max-w-7xl mx-auto">
+    <h2>Hello</h2>
+    <div v-if="true">
+      <h1>{{ user?.name }}</h1>
+      <p>{{ user?.email }}</p>
+    </div>
+    <div v-else>
+      <h1>Go and login</h1>
+    </div>
+  </div>
+</template>
