622c82863303e5c624521ff81710dcaeb58810ac
Marcus Reis <mvsantosreis@gmail.com>
2024-05-08 07:41:04 +0100

    Logout

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <script setup>
-// import { useAuthStore } from '../stores/auth'
+import { useAuthStore } from '../stores/auth'
 
-// const authStore = useAuthStore()
+const authStore = useAuthStore()
 </script>
 <template>
   <nav class="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
@@ -50,7 +50,7 @@
               >Home</router-link
             >
           </li>
-          <template v-if="true">
+          <template v-if="!authStore.user">
             <li>
               <router-link
                 :to="{ name: 'Login' }"
@@ -68,6 +68,7 @@
           </template>
           <template v-else>
             <button
+              @click="authStore.handleLogout"
               class="block rounded py-2 pr-4 pl-3 text-gray-50 hover:bg-gray-700 md:border-0"
             >
               Logout
